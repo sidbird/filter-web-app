46411c4c1dec95512dc4d306906fb16c0e4bfe57
sidbird <123426974+sidbird@users.noreply.github.com>
2023-03-06 17:50:53 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -24,3 +24,10 @@ function gotPoses(results){
         console.log("nose y =" + results[0].pose.nose.y);
     }
 }
+
+function draw(){image(video, 0, 0, 300, 300);}
+    function take_snapshot(){
+        save('myfilterImage.png');
+    }
+
+    
