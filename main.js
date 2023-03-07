10bf413358591d11ad6fe7d7c7859330484d0af9
sidbird <123426974+sidbird@users.noreply.github.com>
2023-03-06 19:01:21 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+noseX=0;
+noseY=0;
 function preload(){
-
+clown_nose = loadImage('https://i.postimg.cc/3x3QzSGq/m.png');
 }
 function setup(){
     canvas = createCanvas(300, 300);
@@ -15,12 +17,21 @@ function setup(){
 function modelLoaded() {
     console.log('PoseNet Is Initialized');
 }
-
 function gotPoses(results){
     if (results.length > 0)
     {
         console.log(results);
-        console.log("nose x = " + results[0].pose.nose.x);
-        console.log("nose y =" + results[0].pose.nose.y);
+        noseX=results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x = " + noseX);
+        console.log("nose y =" + noseY);
     }
+}
+
+function draw(){
+    image(video,0, 0, 300, 300);
+    //fill(255,0,0);
+    //stroke(255,0,0);
+    //circle(noseX, noseY, 20);
+image(clown_nose, noseX-27, noseY-5, 50, 50);
 }
